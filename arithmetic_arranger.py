08012a6bdb45f87f22378665a6953cafd22bce28
diogoguimaraesr <diogo.m.guimaraes.92@gmail.com>
2021-01-14 09:33:00 +0000

    implementation start

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,45 @@
-def arithmetic_arranger(problems):
+MAX_OPERAND_SIZE = 4
 
+def arithmetic_arranger(problems, is_solve=False):
 
-    return arranged_problems
+    result = ""
+    problems_size = len(problems)
+    if problems_size <= 5:
+      operands = []
+      operator = [] 
+
+      for problem in problems:
+        splitted_problem = problem.split()
+        unverified_operands = [splitted_problem[0], splitted_problem[2]]
+        is_valid, result = validate_problem(unverified_operands, splitted_problem[1])
+
+        if is_valid == True:
+          operands.append(unverified_operands)
+          operator.append(splitted_problem[1])
+        else:
+          break      
+    else:
+      result = "Error: Too many problems."
+
+    return result
+
+def validate_problem(operands, operator):
+
+  is_valid = True
+  result = ""
+
+  for operand in operands:
+    if len(operand) > MAX_OPERAND_SIZE:
+      result = "Error: Numbers cannot be more than four digits."
+      is_valid = False
+      break
+    elif operand.isdigit() == False:
+      result = "Error: Numbers must only contain digits."
+      is_valid = False
+      break
+
+  if is_valid and operator != "+" and operator != "-":
+    is_valid = False
+    result = "Error: Operator must be '+' or '-'."
+
+  return is_valid, result
--- main.py
+++ main.py
@@ -4,7 +4,10 @@ from unittest import main
 
 
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
-
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print(arithmetic_arranger(["32d + 698", "3801 - 2", "45 + 43", "123 + 49"], True))
+print(arithmetic_arranger(["32 + 698", "38014 - 2", "45 + 43", "123 + 49"], True))
+print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49", "123 + 49", "123 + 49"], True))
 
 # Run unit tests automatically
 main(module='test_module', exit=False)
